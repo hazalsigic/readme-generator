440413f05b0a39adc8263a4b8f88a925a8126053
hazalsigic <hazalsigic@hotmail.com>
2024-02-18 21:16:47 +0000

    readme generator js codes added

--- index.js
+++ index.js
@@ -5,16 +5,78 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
-];
+    {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Provide description for the project',
+},
+{
+    type: 'input',
+    name: 'installation',
+    message: 'installation instructions?',
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: 'usage information?',
+},
+{
+    type: 'list',
+    name: 'License',
+    message: 'Select the license',
+    choices: [
+    'Apache License',
+    'GNU General Public License',
+    'MIT License',
+    'BSD 2-Clause',
+    'BSD 3-Clause',
+    'Boost',
+    'Creative Commons Zero',
+    'Eclipse',
+    'Mozilla',
+    ]
+},
+{
+    type: 'input',
+    name: 'contributing',
+    message: 'Would you like to add contributors?',
+},
+{
+    type: 'input',
+    name: 'tests',
+    message: 'test instructions?',
+},
+{
+    type: 'input',
+    name: 'username',
+    message: 'What is your github username?',
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+},];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) 
+    {
+        fileName = `README.md`;
+    
+        fs.writeFile(fileName, generateMarkdown(data), (err) =>
+          err ? console.log(err) : console.log('Success!')
+        )
+    }
 
 // function to initialize program
 function init() {
-
+    inquirer
+    .prompt(questions)
+    .then(answers => writeToFile('readme.md', answers));
 }
 
 // function call to initialize program
